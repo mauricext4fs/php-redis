ed7e646abd3fcfb92c77278d00f2205babbf9a0e
Maurice Courtois <courtoisninja@gmail.com>
2014-06-29 08:57:38 -0400

    - Moved from phpRedisController to php-redis, all this for PHP Composer

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-php-redis
-=========
+php-redis-controller
+====================
 
-PHP Redis Client
+Simple but efficient PHP Redis Controller. Written purely in PHP. Do not requires any Redis PHP extension.
+
+As of today, and for the best performance, this is mean only to be use with a Redis server install locally with unix socket enabled.
+
+The location of that unix socket must be as follow: /tmp/redis.sock
---
+++ src/Mauricext4fs/Redis/RedisController.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * See licensing info in ../../LICENSE
+ */
+namespace Mauricext4fs\Redis;
+
+class RedisController {
+
+    private  $objServer = null;
+
+    public function __construct()
+    {
+    
+    }
+
+    public function getList($strKey, $numLimit = 0)
+    {
+        $arrReturn = array();
+        $objServer = $this->getInstance();
+
+        $strMsg = sprintf("LRANGE %s %d %d", $strKey, ($numLimit) ? $numLimit * -1 : -10000, ($numLimit) ? $numLimit : 10000);
+        $strResponse = $this->sendFormattedCommand($strMsg);
+        $numResult = intval(str_replace("*", "", $strResponse));
+        for ($i=0; $i<$numResult; $i++) {
+            // Geting the length of result from response
+            $strResponse = fgets($objServer);
+            $numLength = intval(str_replace("$", "", $strResponse));
+            // Getting the actual value
+            $strResponse = fread($objServer, $numLength);
+            // Remove enclosing quotes from value
+            //$strResponse = substr($strResponse, 1, -1);
+            // Getting the carriage return and disregard it
+            $strAbfall = fgets($objServer);
+            // Adding the value to the array
+            $arrReturn[] = $strResponse;
+        }
+        return $arrReturn;
+    }
+
+    public function addValueToEndOfList($strKey, $strValue)
+    {
+        $objServer = $this->getInstance();
+        
+        $strMsg = sprintf("RPUSH %s %s", $strKey, $strValue);
+        $this->sendFormattedCommand($strMsg);
+        
+        return;
+    }
+
+    public function sendFormattedCommand($strCmd)
+    {
+        $objServer = $this->getInstance();
+
+        $strCmd = sprintf("%s\r\n", $strCmd);
+        fputs($objServer, $strCmd);
+        $strResponse = fgets($objServer);
+
+        return $strResponse;
+    }
+
+
+    protected function getInstance()
+    {
+        if(empty($this->objServer)) {
+            // Connecting
+            if (!file_exists("/tmp/redis.sock")) {
+                throw new \Exception("/tmp/redis.sock not existing");
+            }
+            $this->objServer = stream_socket_client("unix:///tmp/redis.sock", $errno, $errstr, 30);
+            // Test connection
+            $this->sendFormattedCommand("ping");
+            if ($errno) {
+                throw new Exception(sprintf("%s\n", $errno));
+            }
+            if ($errstr) {
+               throw new Exception( printf("%s\n", $errstr));
+            }
+        }
+
+        return $this->objServer;
+    }
+}
